5e0dee35c064a84505c22e7584151a0eadf5e557
Michael J. Lyons <mikelyons7@me.com>
2018-06-27 15:01:45 -0600

    Update color to bike and changed block to trail

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -4,7 +4,7 @@ const GamePiece = require('../lib/GamePiece.js');
 describe ('GamePiece', function () {
 
   it('should have properties', function() {
-    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(0, 222, 254)', 1);
 
     assert.deepEqual(gamePiece, {
       x: 50,
@@ -18,7 +18,7 @@ describe ('GamePiece', function () {
   })
 
   it('should be able to collide with other objects', function() {
-    const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+    const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(0, 222, 254)', 1);
     const gamePiece2 = new GamePiece(58, 58, 10, 10, 'rgb(250, 0, 0)', 1);
 
     const isColliding = gamePiece1.isCollidingWith(gamePiece2);
@@ -27,7 +27,7 @@ describe ('GamePiece', function () {
   })
 
   it('should not collide with objects when they dont overlap', function() {
-    const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+    const gamePiece1 = new GamePiece(50, 50, 10, 10, 'rgb(0, 222, 254)', 1);
     const gamePiece2 = new GamePiece(65, 65, 10, 10, 'rgb(250, 0, 0)', 1);
 
     const isColliding = gamePiece1.isCollidingWith(gamePiece2);
@@ -36,7 +36,7 @@ describe ('GamePiece', function () {
   })
 
   it('should be able to move', function() {
-    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0)', 1);
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(0, 222, 254)', 1);
 
     gamePiece.move();
     
