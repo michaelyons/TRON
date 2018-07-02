8de2bb2ac36be4147436577a9c42076c8c7dd3f1
Michael J. Lyons <mikelyons7@me.com>
2018-07-02 11:35:49 -0600

    Add test for animate, constructor, and trail collision in game file

--- test/Game-test.js
+++ test/Game-test.js
@@ -3,13 +3,43 @@ const Game = require('../lib/Game.js');
 
 describe ('Game', function () {
 
-  it('', function() {
+  it('should have properties', function() {
     assert.deepEqual(, {
 
     })
   });
 
-  it('', function() {
+  it('should be able to draw', function() {
+    assert.deepEqual(, {
+      
+    })
+  });
+
+  it('game should be able to move once drawn', function() {
+    assert.deepEqual(, {
+      
+    })
+  });
+
+  it('should have a trail', function() {
+    assert.deepEqual(, {
+      
+    })
+  });
+
+  it('should collide if player collides with other player trail', function() {
+    assert.deepEqual(, {
+      
+    })
+  });
+
+  it('should stop moving once collision is detected', function() {
+    assert.deepEqual(, {
+      
+    })
+  });
+
+  it('should increment score of winner by 1', function() {
     assert.deepEqual(, {
       
     })
