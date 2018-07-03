996f7967953368214a29f1c2dae581a025fa7d8e
sojurner <pykim0591@gmail.com>
2018-07-02 19:55:57 -0600

    changed gamePiece Test file

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -20,11 +20,11 @@ describe ('GamePiece', function () {
 
   it('should be able to collide with other objects', function() {
     const gamePiece1 = new GamePiece(50, 50, 10, 5, 'rgb(0, 222, 254)', 1);
-    const gamePiece2 = new GamePiece(58, 58, 10, 5, 'rgb(250, 0, 0)', 1);
+    const gamePiece2 = new GamePiece(51, 51, 10, 5, 'rgb(250, 0, 0)', 1);
 
     const isColliding = gamePiece1.isCollidingWith(gamePiece2);
     
-    assert.isFalse(isColliding);
+    assert.equal(isColliding, true);
   })
 
   it('should not collide with objects when they dont overlap', function() {
@@ -33,14 +33,13 @@ describe ('GamePiece', function () {
 
     const isColliding = gamePiece1.isCollidingWith(gamePiece2);
     
-    assert.isFalse(isColliding);
+    assert.equal(isColliding, false);
   })
 
   it('should be able to move', function() {
     const gamePiece = new GamePiece(50, 50, 10, 5, 'rgb(0, 222, 254)', 1);
-
     gamePiece.move();
-    
     assert.equal(gamePiece.x, 51);
+    assert.equal(gamePiece.y, 50);
   })
 })
