78dfa70c1f764ef5fafbe9fb736d5693e32c060f
sojurner <pykim0591@gmail.com>
2018-07-02 22:15:02 -0600

    testing direction

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,53 +1,63 @@
-// const { assert } = require('chai');
-// const Game = require('../lib/Game.js');
+const { assert } = require('chai');
+const Game = require('../lib/Game.js');
 
-// describe ('Game', function () {
+describe ('Game', function () {
 
-//   it('should have properties', function() {
-//     assert.deepEqual(, {
+  it('should have properties', function() {
+    const game = new Game()
+    assert.deepEqual(game, {
+        isGameOver: false,
+        moving: false,
+        stopped: false,
+        player1: {x: 30, y: 50, height: 1, width: 1, color: 'rgb(92,247,249)', dx: 1, dy: 0, dxv: 1},
+        player2: {x: 270, y: 50, height: 1, width: 1, color: 'rgb(255,33,49)', dx: -1, dy: 0, dxv: 1},
+        player1Score: 0,
+        player2Score: 0, 
+        bikes: [{x: 30, y: 50, height: 1, width: 1, color: 'rgb(92,247,249)', dx: 1, dy: 0, dxv: 1}, {x: 270, y: 50, height: 1, width: 1, color: 'rgb(255,33,49)', dx: -1, dy: 0, dxv: 1}],
+        keyPressed: false,
+        trail1: [],
+        trail2: []
+    })
+  })
 
-//     })
-//   });
+  it('should be able to draw two players and move', function() {
+    const gameA = new Game()   
+    
+    assert.deepEqual(gameA.player1.height, 1)  
+    assert.deepEqual(gameA.player2.width, 1)
 
-//   it('should be able to draw', function() {
-//     assert.deepEqual(, {
+    gameA.player1.move();
+    gameA.player2.move();
+
+    assert.equal(gameA.player1.x, 31)
+    assert.equal(gameA.player2.x, 269)
+    })
+
+    it('each player should have a trail', function() {
+      const gameB = new Game()  
       
-//     })
-//   });
+      gameB.player1.move();
+        gameB.player2.move();
+      gameB.createTrail()
 
-//   it('game should be able to move once drawn', function() {
-//     assert.deepEqual(, {
-      
-//     })
-//   });
+      assert.deepEqual(gameB.trail1[0], { x: 31, y: 50, height: 1, width: 1 })
+      assert.deepEqual(gameB.trail2[0], { x: 269, y: 50, height: 1, width: 1 })
+    
+      gameB.player1.move();
+        gameB.player2.move();
+        gameB.createTrail()
 
-//   it('should have a trail', function() {
-//     assert.deepEqual(, {
-      
-//     })
-//   });
+        assert.deepEqual(gameB.trail1[0], { x: 32, y: 50, height: 1, width: 1 })
+      assert.deepEqual(gameB.trail2[0], { x: 268, y: 50, height: 1, width: 1 })
 
-//   it('should detect collision if player collides with other player trail and cease animation', function() {
-//     assert.deepEqual(, {
-      
-//     })
-//   });
+    })
 
-//   it('should increment score of winner by 1', function() {
-//     assert.deepEqual(, {
-      
-//     })
-//   });
+    // it('each player should detect collision', function() {
+    //    const gameC = new Game()
+       
+    //    gameC.trailCollision()
 
-//   it('should allow players to collide with one another', function() {
-//     assert.deepEqual(, {
-      
-//     })
-//   });
-
-//   it('should detect collision with walls and cease animation', function() {
-//     assert.deepEqual(, {
-      
-//     })
-//   });
-// })
+    //   assert.deepEqual(gameB.trail1[0], { x: 31, y: 50, height: 1, width: 1 })
+       
+    // })
+})
